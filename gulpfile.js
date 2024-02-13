4f4d5ab957894be0e395f31914f004f46d679177
Marcell Lindenborg <ml@pintle.dk>
2024-02-13 11:57:46 +0100

    optimizations

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,14 @@ const postcss = require("gulp-postcss");
 const cssnano = require("cssnano");
 const terser = require("gulp-terser");
 const browsersync = require("browser-sync").create();
+const gzip = require("gulp-gzip");
 
 // Sass Task
 function scssTask() {
   return src("assets/scss/**/*.scss", { sourcemaps: true })
     .pipe(sass())
     .pipe(postcss([cssnano()]))
+    .pipe(gzip())
     .pipe(dest("dist", { sourcemaps: "." }));
 }
 
@@ -17,6 +19,7 @@ function scssTask() {
 function jsTask() {
   return src("assets/js/**/*.js", { sourcemaps: true })
     .pipe(terser())
+    .pipe(gzip())
     .pipe(dest("dist", { sourcemaps: "." }));
 }
 
